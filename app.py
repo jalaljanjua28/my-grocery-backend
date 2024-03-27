0e04d40dd4c16d5295f874e8303f4940842ce1d6
jalaljanjua28 <jalaljanjua88@gmail.com>
2024-03-27 19:29:37 +0500

    new file:   __pycache__/app.cpython-39.pyc 	modified:   app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 
 from dateparser.search import search_dates
-from google.cloud import secretmanager
+# from google.cloud import secretmanager
 
 app = Flask(__name__)
 CORS(app, methods=["GET", "POST"])
@@ -36,9 +36,7 @@ language = "eng"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "application_default_credentials.json"
 os.environ["GOOGLE_CLOUD_PROJECT"] = "my-grocery-home"
 os.environ["BUCKET_NAME"] = "grocery-bucket"
-storage_client = storage.Client.from_service_account_json(
-    "my-grocery-home-745726ebbfac.json"
-)
+
 
 load_dotenv()
 
@@ -58,17 +56,33 @@ storage_client = storage.Client()
 text = ""
 date_record = list()
 
-project_id = "my-grocery-home"
-secret_id = "Credentials"
+def load_credentials():
+    with open('my-grocery-home-745726ebbfac.json') as f:
+        return json.load(f)
 
-def get_secret(secret_id, project_id):
-    client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
-    response = client.access_secret_version(name=name)
-    payload = response.payload.data.decode("UTF-8")
-    return payload
+credentials = load_credentials()
+storage_client = storage.Client(credentials=credentials)
 
-credentials_json = get_secret(secret_id, project_id)
+# project_id = "my-grocery-home"
+# secret_id = "credentials"
+# # Initialize Secret Manager client
+# client = secretmanager.SecretManagerServiceClient()
+
+# # Define the name of the secret you want to access
+# secret_name = "projects/project_id/secrets/secret_id/versions/2"
+
+# # Access the secret
+# response = client.access_secret_version(request={"name": secret_name})
+
+# # Retrieve the secret payload
+# payload_data = response.payload.data.decode("UTF-8")
+# print(f"Retrieved secret value: {payload_data}")
+
+# Assuming the payload contains JSON with an access token
+# secret_dict = json.loads(payload_data)
+# access_token = secret_dict["access_token"]
+
+# print("Access Token:", access_token)
 
 #                           ChatGpt Prompts Section
 # Homepage (cooking_tips, current_trends, ethical_eating_suggestions, food_waste_reductions,
